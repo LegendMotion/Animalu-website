8294496aa2800bd947f553d8543d26c0e5aecd82
naleyjanhelge <naleyjanhelge@gmail.com>
2026-01-02 15:04:10 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     const cid = yt.dataset.youtubeChannel;
     const iframe = document.createElement('iframe');
     iframe.loading = 'lazy';
-    iframe.height = '315';
+    iframe.height = '360';
     iframe.src = `https://www.youtube.com/embed?listType=playlist&list=UU${cid.replace(/^UC/,'')}`;
     iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
     yt.appendChild(iframe);
